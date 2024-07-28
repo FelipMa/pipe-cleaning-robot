094c81d9cb019bc8d8922fa1a495ec7bee8a8000
FelipMa <felipeapenburg@gmail.com>
2024-07-28 01:00:50 -0300

    refactor graphics

--- graphics.v
+++ graphics.v
@@ -12,17 +12,18 @@ parameter WALL_X_L = 30;
 parameter WALL_X_R = 40;
 
 // internal wires
-reg robot_on, block_on, empty_block_on;
+reg robot_on, free_path_block_on, wall_block_on;
 
 // internal registers
 reg [7:0] r_next, g_next, b_next;
+
 // assignments
 // map __________________________________________________________________
-wire [3:0] map_mem; 
-wire [5:0] map_index;
+wire [3:0] map_y; 
+wire [5:0] map_x;
 reg [0:59] map_data;
-always @* begin
-	case(map_mem)
+always @(map_x or map_y) begin
+	case(map_y)
 		4'd0: map_data =  60'o0000_0000_0111_1110_0000;
 		4'd1: map_data =  60'o0000_0000_0100_0010_0000;
 		4'd2: map_data =  60'o0000_0100_0100_0011_1111;
@@ -38,25 +39,27 @@ always @* begin
 		4'd12: map_data = 60'o0000_0000_0000_0000_0000;
 		4'd13: map_data = 60'o0000_0000_0000_0000_0000;
 		4'd14: map_data = 60'o0000_0000_0000_0000_0000;
-		4'd15: map_data = 60'o0000_0000_0000_0000_0000;
-		endcase
-	case(map_data[map_index +: 3])
-		3'd0: begin empty_block_on = 1'b1; block_on = 1'b0; robot_on = 1'b0; end
-		3'd1: begin empty_block_on = 1'b0; block_on = 1'b1; robot_on = 1'b0; end
-		3'd2: begin empty_block_on = 1'b0; block_on = 1'b0; robot_on = 1'b1; end
-		3'd3, 3'd4, 3'd5, 3'd6, 3'd7: begin empty_block_on = 1'b0; block_on = 1'b0; robot_on = 1'b0; end
-		default: begin empty_block_on = 1'b0; block_on = 1'b0; robot_on = 1'b0; end
+		default: map_data = 60'o0000_0000_0000_0000_0000;
+	endcase
+
+	case(map_data[map_x +: 3]) // set flags on according to current map position being drawn
+		3'd0: begin wall_block_on = 1'b1; free_path_block_on = 1'b0; robot_on = 1'b0; end
+		3'd1: begin wall_block_on = 1'b0; free_path_block_on = 1'b1; robot_on = 1'b0; end
+		3'd2: begin wall_block_on = 1'b0; free_path_block_on = 1'b0; robot_on = 1'b1; end
+		default: begin wall_block_on = 1'b0; free_path_block_on = 1'b0; robot_on = 1'b0; end
 	endcase
 end
-assign map_mem = pix_y / 32;
-assign map_index = (pix_x / 32)* 3;
+// map x (0:19) and y (0:14) represent the coordinates of the map blocks
+assign map_y = pix_y / 32;
+assign map_x = (pix_x / 32) * 3; // (*3 is because each block is represented by 3 bits)
 // map __________________________________________________________________
+
 // robot ________________________________________________________________
 reg [0:95] robot_data;
-wire [4:0] robot_mem;
-wire [6:0] robot_index;
-always @* begin
-	case(robot_mem) // ROM memory of Wall-e pixels
+wire [4:0] robot_block_y;
+wire [6:0] robot_block_x;
+always @(robot_block_y) begin
+	case(robot_block_y) // ROM memory of Wall-e pixels
 		5'd0: robot_data =  96'o1111_1111_1111_1111_1111_1111_1111_1111;
 		5'd1: robot_data =  96'o1000_0000_0000_0000_0000_0000_0000_0001;
 		5'd2: robot_data =  96'o1000_0000_0000_0000_0000_0000_0000_0001;
@@ -92,134 +95,138 @@ always @* begin
 	endcase
 end
 
-assign robot_mem = pix_y % 32;
-assign robot_index = (pix_x % 32)* 3; //& {robot_on, robot_on, robot_on, robot_on, robot_on, robot_on, robot_on};
-
+// robot x and y go from 0 to 31 and represent relative position of the pixel on the 32x32 block
+assign robot_block_y = pix_y % 32;
+assign robot_block_x = (pix_x % 32) * 3; // (*3 is because each color is represented by 3 bits)
 // robot ________________________________________________________________
-// empty_block __________________________________________________________
-wire [4:0] empty_block_mem, empty_block_index;
-reg [0:31] empty_block_data;
-always @* begin
-	case(empty_block_mem)
-		5'd0: empty_block_data =  32'b1111_1111_1111_1111_1111_1111_1111_1111;
-		5'd1: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd2: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd3: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd4: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd5: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd6: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd7: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd8: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd9: empty_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd10: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd11: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd12: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd13: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd14: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd15: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd16: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd17: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd18: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd19: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd20: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd21: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd22: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd23: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd24: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd25: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd26: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd27: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd28: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd29: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd30: empty_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd31: empty_block_data = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
-		endcase
-end
-assign empty_block_mem = pix_y % 32;
-assign empty_block_index = pix_x % 32;
-// empty_block __________________________________________________________
-// block ________________________________________________________________
-wire [4:0] block_mem, block_index;
-reg [0:31] block_data;
-always @* begin
-	case(block_mem)
-		5'd0: block_data =  32'b1111_1111_1111_1111_1111_1111_1111_1111;
-		5'd1: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd2: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd3: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd4: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd5: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd6: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd7: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd8: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd9: block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd10: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd11: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd12: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd13: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd14: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd15: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd16: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd17: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd18: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd19: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd20: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd21: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd22: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd23: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd24: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd25: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd26: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd27: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd28: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd29: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd30: block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
-		5'd31: block_data = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
-		endcase
-end
-assign block_mem = pix_y % 32;
-assign block_index = pix_x % 32;
-// block ________________________________________________________________
 
+// wall_block __________________________________________________________
+wire [4:0] wall_block_y, wall_block_x;
+reg [0:31] wall_block_data;
+always @(wall_block_y) begin
+	case(wall_block_y)
+		5'd0: wall_block_data =  32'b1111_1111_1111_1111_1111_1111_1111_1111;
+		5'd1: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd2: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd3: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd4: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd5: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd6: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd7: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd8: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd9: wall_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd10: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd11: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd12: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd13: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd14: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd15: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd16: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd17: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd18: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd19: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd20: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd21: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd22: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd23: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd24: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd25: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd26: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd27: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd28: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd29: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd30: wall_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd31: wall_block_data = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+	endcase
+end
+// wall_block y and x go from 0 to 31 and represent relative position of the pixel on the 32x32 block
+assign wall_block_y = pix_y % 32;
+assign wall_block_x = pix_x % 32; // colors here are represented by 1 bit
+// wall_block __________________________________________________________
 
-always @(video_on or robot_on or robot_index or robot_data or block_on or empty_block_on or block_data or empty_block_data or block_index or empty_block_index) begin
+// free_path_block ________________________________________________________________
+wire [4:0] free_path_block_y, free_path_block_x;
+reg [0:31] free_path_block_data;
+always @(free_path_block_y) begin
+	case(free_path_block_y)
+		5'd0: free_path_block_data =  32'b1111_1111_1111_1111_1111_1111_1111_1111;
+		5'd1: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd2: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd3: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd4: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd5: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd6: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd7: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd8: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd9: free_path_block_data =  32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd10: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd11: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd12: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd13: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd14: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd15: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd16: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd17: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd18: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd19: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd20: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd21: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd22: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd23: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd24: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd25: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd26: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd27: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd28: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd29: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd30: free_path_block_data = 32'b1000_0000_0000_0000_0000_0000_0000_0001;
+		5'd31: free_path_block_data = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+	endcase
+end
+// free_path_block x and y go from 0 to 31 and represent relative position of the pixel on the 32x32 block
+assign free_path_block_y = pix_y % 32;
+assign free_path_block_x = pix_x % 32; // colors here are represented by 1 bit
+// free_path_block ________________________________________________________________
+
+always @* begin
 	if (~video_on) begin
-		r_next = 8'd0;
+        // emit pink when not in video_on
+		r_next = 8'd255;
 		g_next = 8'd0;
-		b_next = 8'd0;
+		b_next = 8'd255;
 		end
 	else if(robot_on) begin
-		case(robot_data[robot_index +: 3]) 
-		3'd0: begin r_next = 8'd190; g_next = 8'd190; b_next = 8'd190; end //grey
-		3'd1: begin r_next = 8'd0; g_next = 8'd0; b_next = 8'd0; end //black
-		3'd2: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end //white
-		3'd3: begin r_next = 8'd168; g_next = 8'd168; b_next = 8'd168; end //grey(darker)
-		3'd4: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd0; end //yellow
-		3'd5: begin r_next = 8'd255; g_next = 8'd0; b_next = 8'd0; end //red
-		3'd6: begin r_next = 8'd92; g_next = 8'd64; b_next = 8'd51; end //brown
-		default: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end// white
+		case(robot_data[robot_block_x +: 3]) 
+            3'd0: begin r_next = 8'd190; g_next = 8'd190; b_next = 8'd190; end //grey
+            3'd1: begin r_next = 8'd0; g_next = 8'd0; b_next = 8'd0; end //black
+            3'd2: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end //white
+            3'd3: begin r_next = 8'd168; g_next = 8'd168; b_next = 8'd168; end //grey (darker)
+            3'd4: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd0; end //yellow
+            3'd5: begin r_next = 8'd255; g_next = 8'd0; b_next = 8'd0; end //red
+            3'd6: begin r_next = 8'd92; g_next = 8'd64; b_next = 8'd51; end //brown
+            default: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end // white
 		endcase
 	end
-	else if(block_on) begin
-		case(block_data[block_index])
-		1'b0: begin r_next = 8'd190; g_next = 8'd190; b_next = 8'd190; end // grey
-		1'b1: begin r_next = 8'd0; g_next = 8'd0; b_next = 8'd0; end //black
-		default: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end //white
+	else if(free_path_block_on) begin
+		case(free_path_block_data[free_path_block_x])
+            1'b0: begin r_next = 8'd190; g_next = 8'd190; b_next = 8'd190; end // grey
+            1'b1: begin r_next = 8'd0; g_next = 8'd0; b_next = 8'd0; end //black
+            default: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end //white
 		endcase
 	end
-	else if(empty_block_on) begin
-		case(empty_block_data[empty_block_index])
-		1'b0: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end // white
-		1'b1: begin r_next = 8'd0; g_next = 8'd0; b_next = 8'd0; end //black
-		default: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end //white
+	else if(wall_block_on) begin
+		case(wall_block_data[wall_block_x])
+            1'b0: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end // white
+            1'b1: begin r_next = 8'd0; g_next = 8'd0; b_next = 8'd0; end //black
+            default: begin r_next = 8'd255; g_next = 8'd255; b_next = 8'd255; end //white
 		endcase
 	end
 	else begin
+        // emit red when not in any block
 		r_next = 8'd255;
-		g_next = 8'd255;
-		b_next = 8'd255;
+		g_next = 8'd0;
+		b_next = 8'd0;
 	end
-	
 end
 
 always @(posedge clock_50) begin
