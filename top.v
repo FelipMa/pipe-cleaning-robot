599ec82f7d76b0137006b3300f8d2a10ae4560b8
FelipMa <felipeapenburg@gmail.com>
2024-08-09 22:48:37 -0300

    :bug: fix vga and pll

--- top.v
+++ top.v
@@ -1,9 +1,10 @@
-module top (CLOCK_50, KEY, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B);
+module top (CLOCK_50, KEY, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B, VGA_CLK, LEDG);
 
 input wire CLOCK_50;
 input wire [3:0] KEY;
-output wire VGA_HS, VGA_VS;
+output wire VGA_HS, VGA_VS, VGA_CLK;
 output wire [7:0] VGA_R, VGA_G, VGA_B;
+output wire [8:0] LEDG;
 
 // internal wires
 wire pll_clk_25;
@@ -11,7 +12,7 @@ wire [9:0] pixel_x, pixel_y;
 wire video_on;
 
 // build PLL
-pll	pll (.inclk0(CLOCK_50), .areset(KEY[2]), .c0(pll_clk_25));
+pll	pll (.inclk0(CLOCK_50), .areset(1'b0), .c0(pll_clk_25));
 
 // build world
 world world (.clock_50(CLOCK_50), .reset_key(KEY[0]));
@@ -21,5 +22,8 @@ vga_sync vga_sync(.clock_50(CLOCK_50), .clock_25(pll_clk_25), .reset_key(KEY[1])
 
 // build the graphics
 graphics graphics(.clock_50(CLOCK_50), .video_on(video_on), .pix_x(pixel_x), .pix_y(pixel_y), .graph_r(VGA_R), .graph_g(VGA_G), .graph_b(VGA_B));
+
+assign VGA_CLK = pll_clk_25;
+assign LEDG[0] = 1'b1;
 
 endmodule
