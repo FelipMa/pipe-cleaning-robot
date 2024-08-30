60ff10aa1cdecb075ede7848c1b910c158f79009
Miguel Mota <94927896+miguelmota2301@users.noreply.github.com>
2024-08-30 10:29:03 -0300

    Controller update
    The code for the controller was wrong (old code without the right FSM) and Saidas (buttons output) was [10:0] (the right one is [11:0].
    Top has changed accordingly.

--- controller.v
+++ controller.v
@@ -1,144 +1,156 @@
-// SEGA Genesis Controller PinOut
-// 1 - Up/Z
-// 2 - Down/Y
-// 3 - Left/X
-// 4 - Right
-// 5 - +5V
-// 6 - A/B
-// 7 - Select Signal
-// 8 - Ground
-// 9 - Start/C
+module controller (clock_50, reset, Pino1, Pino2, Pino3, Pino4, Pino6, Pino9, vga_vs, Saidas, Select);
 
-/*
-buttonsIn: Pin DB-9 do controle
-buttonsOut: Saída que o módulo top-level vai receber
-*/
+input clock_50, reset, Pino1, Pino2, Pino3, Pino4, Pino6, Pino9, vga_vs;
+output reg [11:0] Saidas;
+output reg Select;
+
+// Saidas[0] = Saida_Mode
+// Saidas[1] = Saida_Start
+// Saidas[2] = Saida_Z
+// Saidas[3] = Saida_Y
+// Saidas[4] = Saida_X
+// Saidas[5] = Saida_C
+// Saidas[6] = Saida_B
+// Saidas[7] = Saida_A
+// Saidas[8] = Saida_Right
+// Saidas[9] = Saida_Left
+// Saidas[10] = Saida_Down
+// Saidas[11] = Saida_Up
 
 
-module controller (
-// Outputs ==================
-output reg [10:0] buttonsOut,
-output reg selectSignal,
-//===========================
-
-// Inputs ===========
-input wire clk,
-		reset,
-		up_z,
-		down_y,
-		left_x,
-		right,
-		a_b,
-		start_c,
-		vga_vs
-//===================
-);
-
-// Internal Wires ================
-wire 	up_z_debounced,
-		down_y_debounced,
-		left_x_debounced,
-		right_debounced,
-		a_b_debounced,
-		start_c_debounced,
-		Flag;
-//================================
-		
-// Internal Regs ==================
-reg 	vga_vs_primeiro_FF,
-		vga_vs_segundo_FF;
-		
-reg [3:0] Estado_Atual, Estado_Futuro;
+parameter	AGUARDAR_ATIVACAO = 0,
+				ESTADO_0 = 1,
+				ESTADO_1 = 2,
+				ESTADO_2 = 3,
+				ESTADO_3 = 4,
+				ESTADO_4 = 5,
+				ESTADO_5 = 6,
+				ESTADO_6 = 7,
+				ESTADO_7 = 8;
+				
+reg [3:0] EstadoAtual, EstadoFuturo;
 
 reg [12:0] Contador;
-//================================
-		
-// Internal Parameters ============		
-parameter AGUARDAR_ATIVACAO = 0,
-				SELECT_0 = 1,
-				SELECT_1 = 2,
-				AGUARDAR_CONTADOR = 3;
-//=================================
 
-// Instatiation ================================================================================
-debouncer db_up_z(.clk(clk), .reset(reset), .noisy(up_z), .debounced(up_z_debounced));
-debouncer db_down_y(.clk(clk), .reset(reset), .noisy(down_y), .debounced(down_y_debounced));
-debouncer db_left_x(.clk(clk), .reset(reset), .noisy(left_x), .debounced(left_x_debounced));
-debouncer db_right(.clk(clk), .reset(reset), .noisy(right), .debounced(right_debounced));
-debouncer db_a_b(.clk(clk), .reset(reset), .noisy(a_b), .debounced(a_b_debounced));
-debouncer db_start_c(.clk(clk), .reset(reset), .noisy(start_c), .debounced(start_c_debounced));
-//==============================================================================================
+// Flag baseado na deteccao de borda de descida de v_sync
 
-always @(negedge clk) begin
+reg vga_vs_primeiro_FF, vga_vs_segundo_FF;
+wire Flag;
+
+always @(negedge clock_50)
+begin
 	vga_vs_primeiro_FF <= vga_vs;
 	vga_vs_segundo_FF <= vga_vs_primeiro_FF;
 end
 
-assign Flag = ~vga_vs_primeiro_FF & vga_vs_segundo_FF;
+assign Flag = !vga_vs_primeiro_FF && vga_vs_segundo_FF;
 
-always @(posedge clk) begin
-	if (!reset) begin
-		Estado_Atual <= AGUARDAR_ATIVACAO;
+
+always @(posedge clock_50)
+begin
+	if (reset)
+	begin
+		EstadoAtual <= AGUARDAR_ATIVACAO;
 		Contador <= 0;
 	end
-	else begin
-		Estado_Atual <= Estado_Futuro;
+	else
+	begin
+		EstadoAtual <= EstadoFuturo;
 	end
 	
-	if (Estado_Futuro == AGUARDAR_ATIVACAO) begin
+	if (EstadoFuturo == AGUARDAR_ATIVACAO)
+	begin
 		Contador <= 0;
 	end
-	else begin
+	else
+	begin
 		Contador <= Contador + 1;
 	end
 	
-	if (Estado_Futuro == SELECT_0) begin
-		buttonsOut[0] <= up_z_debounced; // Up
-		buttonsOut[1] <= down_y_debounced; // Down
-		buttonsOut[2] <= left_x_debounced; // Left
-		buttonsOut[3] <= right_debounced; // Right
-		buttonsOut[4] <= a_b_debounced; // A
-		buttonsOut[5] <= start_c_debounced;	// Start	
+	if (EstadoFuturo == ESTADO_1)
+	begin
+		Saidas[4] <= !Pino6; // Saida_A
+		Saidas[10] <= !Pino9; // Saida_Start
 	end
 	
-	if (Estado_Futuro == SELECT_1) begin
-		buttonsOut[6] <= up_z_debounced; // Z
-		buttonsOut[7] <= down_y_debounced; // Y
-		buttonsOut[8] <= left_x_debounced; // X
-		buttonsOut[9] <= a_b_debounced; // B
-		buttonsOut[10] <= start_c_debounced; // C		
+	if (EstadoFuturo == ESTADO_2)
+	begin
+		Saidas[0] <= !Pino1; // Saida_Up
+		Saidas[1] <= !Pino2; // Saida_Down
+		Saidas[2] <= !Pino3; // Saida_Left
+		Saidas[3] <= !Pino4; // Saida_Right
+	end
+	
+	if (EstadoFuturo == ESTADO_4)
+	begin
+		Saidas[5] <= !Pino6; // Saida_B
+		Saidas[6] <= !Pino9; // Saida_C
+	end
+	
+	if (EstadoFuturo == ESTADO_6)
+	begin
+		Saidas[7] <= !Pino3; // Saida_X
+		Saidas[8] <= !Pino2; // Saida_Y
+		Saidas[9] <= !Pino1; // Saida_Z
+		Saidas[11] <= !Pino4; // Saida_Mode
 	end
 end
 
-always @(*) begin
-	case (Estado_Atual)
-		AGUARDAR_ATIVACAO: 	if (Flag) Estado_Futuro = SELECT_0;
-									else Estado_Futuro = AGUARDAR_ATIVACAO;
-
-		SELECT_0: 	if (Contador < 3500) Estado_Futuro = SELECT_0;
-						else Estado_Futuro = SELECT_1;
-						
-		SELECT_1:	if (Contador < 7000) Estado_Futuro = SELECT_1;
-						else Estado_Futuro = AGUARDAR_CONTADOR;
-		
-		AGUARDAR_CONTADOR: 	if (Contador < 8000) Estado_Futuro = AGUARDAR_CONTADOR;
-									else Estado_Futuro = AGUARDAR_ATIVACAO;
-									
-		default: Estado_Futuro = AGUARDAR_ATIVACAO;
-	endcase
+// Decodificador de Proximo Estado
+always @(*)
+begin
+	case (EstadoAtual)
+		AGUARDAR_ATIVACAO:	if (Flag)
+										EstadoFuturo = ESTADO_0;
+									else
+										EstadoFuturo = AGUARDAR_ATIVACAO;
+		ESTADO_0:	if (Contador < 1000)
+							EstadoFuturo = ESTADO_0;
+						else
+							EstadoFuturo = ESTADO_1;
+		ESTADO_1:	if (Contador < 2000)
+							EstadoFuturo = ESTADO_1;
+						else
+							EstadoFuturo = ESTADO_2;
+		ESTADO_2:	if (Contador < 3000)
+							EstadoFuturo = ESTADO_2;
+						else
+							EstadoFuturo = ESTADO_3;
+		ESTADO_3:	if (Contador < 4000)
+							EstadoFuturo = ESTADO_3;
+						else
+							EstadoFuturo = ESTADO_4;
+		ESTADO_4:	if (Contador < 5000)
+							EstadoFuturo = ESTADO_4;
+						else
+							EstadoFuturo = ESTADO_5;
+		ESTADO_5: 	if (Contador < 6000)
+							EstadoFuturo = ESTADO_5;
+						else
+							EstadoFuturo = ESTADO_6;
+		ESTADO_6:	if (Contador < 7000)
+							EstadoFuturo = ESTADO_6;
+						else
+							EstadoFuturo = ESTADO_7;
+		ESTADO_7:	if (Contador < 8000)
+							EstadoFuturo = ESTADO_7;
+						else
+							EstadoFuturo = AGUARDAR_ATIVACAO;
+		default:		EstadoFuturo = AGUARDAR_ATIVACAO;
+	endcase 
 end
 
-
-always @(*) begin
-	selectSignal = 0;
-	
-	case(Estado_Atual)
-		SELECT_0: selectSignal = 0;
-
-		SELECT_1: selectSignal = 1;
-
-		default: selectSignal = 0;
-	endcase
+// Decodificador de Saida
+always @(*)
+begin
+	Select = 1;
+	case (EstadoAtual)
+		ESTADO_1: Select = 0;
+		ESTADO_3: Select = 0;
+		ESTADO_5: Select = 0;
+		ESTADO_7: Select = 0;
+		default: Select = 1;
+	endcase	
 end
 
-endmodule
+endmodule 
--- top.v
+++ top.v
@@ -1,13 +1,13 @@
 module top (CLOCK_50, KEY, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B, VGA_CLK, LEDG, SW,
-            up_z, down_y, left_x, right, a_b, selectSignal,	start_c, buttonsOut); //GPIO - Flat IDE de 40 fios. OBS: Ainda não sabemos como são as conexões', então por enquanto vamos deixar assim
+            Pino1, Pino2, Pino3, Pino4, Pino6, Pino9, Saidas, Select);
 
 input wire CLOCK_50;
 input wire [3:0] KEY;
 input wire [17:0] SW;
-// input wire [35:0] GPIO; // input controller
-input wire up_z, down_y, left_x, right, a_b, selectSignal, start_c; // delete after discovering GPIO's connection.
+input wire Pino1, Pino2, Pino3, Pino4, Pino6, Pino9; // buttons input
 output wire VGA_HS, VGA_VS, VGA_CLK;
-output wire [10:0] buttonsOut; // change after integration with robot
+output wire [11:0] Saidas; // buttons output
+output wire Select; // controller select output
 output wire [7:0] VGA_R, VGA_G, VGA_B;
 output wire [8:0] LEDG;
 
@@ -32,7 +32,7 @@ vga_sync vga_sync(.clock_50(CLOCK_50), .clock_25(pll_clk_25), .reset_key(KEY[1])
 graphics graphics(.clock_50(CLOCK_50), .video_on(video_on), .pix_x(pixel_x), .pix_y(pixel_y), .graph_r(VGA_R), .graph_g(VGA_G), .graph_b(VGA_B), .sprite(sprite), .flags(robot_cursor_flags), .robot_type(robot_type));
 
 // build controller
-controller controller(.clk(CLOCK_50), .reset(KEY[0]), .up_z(up_z), .down_y(down_y), .left_x(left_x), .right(right), .a_b(a_b), .selectSignal(selectSignal), .start_c(start_c), .buttonsOut(buttonsOut), .vga_vs(VGA_VS));
+controller controller(.clock_50(CLOCK_50), .reset(KEY[0]), .Pino1(Pino1), .Pino2(Pino2), .Pino3(Pino3), .Pino4(Pino4), .Pino6(Pino6), .Pino9(Pino9), .Saidas(Saidas), .Select(Select), .vga_vs(VGA_VS));
 
 assign LEDG[0] = 1'b1;
 assign VGA_CLK = pll_clk_25;
